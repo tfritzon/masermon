f25bcf72c21701c64aeb2ea63fd465901b44c741
Magnus Danielson <magnus@rubidium.se>
2021-09-03 01:02:47 +0200

    Added identification of data and write data to InfluxDB

--- masermon.py
+++ masermon.py
@@ -1,6 +1,8 @@
 import serial
 import time
 import sys
+from influxdb import InfluxDBClient
+import datetime
 
 SERIALDEVICE = "/dev/ttyUSB0"
 BAUDRATE = 9600
@@ -20,7 +22,11 @@ def poll_chan(ser, chan):
     return ser.read(size=4)
 
 with serial.Serial(SERIALDEVICE, BAUDRATE, timeout=None) as ser:
+    client = InfluxDBClient(host='localhost', port=8086)
+    client.create_database('EFOStest')
+    client.switch_database('EFOStest')
     while True:
+        timestamp = datetime.datetime.utcnow().isoformat()
         for chan in range(0,35):
             r = poll_chan(ser, chan)
             reading = int(r,16)
@@ -65,8 +71,126 @@ with serial.Serial(SERIALDEVICE, BAUDRATE, timeout=None) as ser:
             if signed == True:
                 reading = reading - 128
             value = reading * scale + offset
+            if chan == 0:
+                inputA_U = value
+            elif chan == 1:
+                inputA_I = value
+            elif chan == 2:
+                inputB_U = value
+            elif chan == 3:
+                inputB_I = value
+            elif chan == 4:
+                temp = value
+            elif chan == 5:
+                Hpress_set = value
+            elif chan == 6:
+                Hpress_read = value
+            elif chan == 7:
+                Pall_heat = value
+            elif chan == 8:
+                LO_heat = value
+            elif chan == 9:
+                UO_heat = value
+            elif chan == 10:
+                Dalle_heat = value
+            elif chan == 11:
+                LI_heat = value
+            elif chan == 12:
+                UI_heat = value
+            elif chan == 13:
+                Cavity_heat = value
+            elif chan == 14:
+                temp_cavity = value
+            elif chan == 15:
+                temp_amb = value
+            elif chan == 16:
+                Cavity_var = value
+            elif chan == 17:
+                C_field = value
+            elif chan == 18:
+                intN2HTU = value
+            elif chan == 19:
+                intN2HTI = value
+            elif chan == 20:
+                intN1HTU = value
+            elif chan == 21:
+                intN1HTI = value
+            elif chan == 22:
+                extHTU = value
+            elif chan == 23:
+                extHTI = value
+            elif chan == 24:
+                RFU = value
+            elif chan == 25:
+                RFI = value
+            elif chan == 26:
+                p24V = value
+            elif chan == 27:
+                p15V1 = value
+            elif chan == 28:
+                n15V1 = value
+            elif chan == 29:
+                p5V = value
+            elif chan == 30:
+                p15V2 = value
+            elif chan == 31:
+                n15V2 = value
+            elif chan == 32:
+                OCXO = value
+            elif chan == 33:
+                Ampl57k = value
+            elif chan == 34:
+                lock = value
             # TODO: save in DB
             # print("{%2i} %i %5s %4.1f %6.3f %6.3f" % (chan, reading, signed, offset, scale, value))
             print("{%2i} %9.3f" % (chan, value))
             # time.sleep(0.1)
+        json_body = [
+            {
+                "measurement": "maserdata",
+                "tags": {
+                    "masetype": "EFOS-B",
+                    "maser": "EFOS14"
+                },
+                "time": timestamp,
+                "fields": {
+                    "InputA_U"    : inputA_U,
+                    "InputA_I"    : inputA_I,
+                    "InputB_U"    : inputB_U,
+                    "InputB_I"    : inputB_I,
+                    "Temp"        : temp,
+                    "Hpress_set"  : Hpress_set,
+                    "Hpress_read" : Hpress_read,
+                    "Palladium_heat" : Pall_heat,
+                    "LO_heat"     : LO_heat,
+                    "UO_heat"     : UO_heat,
+                    "Dalle_heat"  : Dalle_heat,
+                    "LI_heat"     : LI_heat,
+                    "UI_heat"     : UI_heat,
+                    "Cavity_heat" : Cavity_heat,
+                    "Temp_cavity" : temp_cavity,
+                    "Temp_ambient" : temp_amb,
+                    "Cavity_var"  : Cavity_var,
+                    "C_field"     : C_field,
+                    "int_N2_HT_U" : intN2HTU,
+                    "int_N2_HT_I" : intN2HTI,
+                    "int_N1_HT_U" : intN1HTU,
+                    "int_N1_HT_I" : intN1HTI,
+                    "ext_HT_U"    : extHTU,
+                    "ext_HT_I"    : extHTI,
+                    "RF_U"        : RFU,
+                    "RF_I"        : RFI,
+                    "p24V"        : p24V,
+                    "p15V1"       : p15V1,
+                    "n15V1"       : n15V1,
+                    "p5V"         : p5V,
+                    "p15V2"       : p15V2,
+                    "n15V2"       : n15V2,
+                    "OCXO"        : OCXO,
+                    "Ampl5.7k"    : Ampl57k,
+                    "Lock"        : lock
+                }
+            }
+        ]
+        client.write_points(json_body)
         time.sleep(10)
