f3896e2799b8f2a40caa722434220aff97809696
Magnus Danielson <magnus@rubidium.se>
2021-09-05 22:14:33 +0200

    Fix table bug. Fix sign bug. Make data table for individual.

--- masermon.py
+++ masermon.py
@@ -14,11 +14,47 @@ if len(sys.argv) > 1:
 if len(sys.argv) > 2:
     BAUDRATE = int(sys.argv[2])
 
-channels = []
+channels = [
+    { "chan": 0,    "name": "InputA_U",       "signed": -128,   "scale": 0.230,   "offset": 0    },
+    { "chan": 1,    "name": "InputA_I",       "signed": -128,   "scale": 0.096,   "offset": 0    },
+    { "chan": 2,    "name": "InputB_U",       "signed": -128,   "scale": 0.230,   "offset": 0    },
+    { "chan": 3,    "name": "InputB_I",       "signed": -128,   "scale": 0.096,   "offset": 0    },
+    { "chan": 4,    "name": "Temp",           "signed": -128,   "scale": 0.960,   "offset": -1.1 },
+    { "chan": 5,    "name": "Hpress_set",     "signed": -128,   "scale": 0.096,   "offset": 0    },
+    { "chan": 6,    "name": "Hpress_read",    "signed": -128,   "scale": 0.096,   "offset": 0    },
+    { "chan": 7,    "name": "Palladium_heat", "signed": -128,   "scale": 0.192,   "offset": 0    },
+    { "chan": 8,    "name": "LO_heat",        "signed": -128,   "scale": 0.192,   "offset": 0    },
+    { "chan": 9,    "name": "UO_heat",        "signed": -128,   "scale": 0.192,   "offset": 0    },
+    { "chan": 10,   "name": "Dalle_heat",     "signed": -128,   "scale": 0.192,   "offset": 0    },
+    { "chan": 11,   "name": "LI_heat",        "signed": -128,   "scale": 0.192,   "offset": 0    },
+    { "chan": 12,   "name": "UI_heat",        "signed": -128,   "scale": 0.192,   "offset": 0    },
+    { "chan": 13,   "name": "Cavity_heat",    "signed": -128,   "scale": 0.192,   "offset": 0    },
+    { "chan": 14,   "name": "Temp_cavity",    "signed": -128,   "scale": 0.010,   "offset": 0    },
+    { "chan": 15,   "name": "Temp_ambient",   "signed": -128,   "scale": 0.096,   "offset": 26   },
+    { "chan": 16,   "name": "Cavity_var",     "signed": -128,   "scale": 0.096,   "offset": 0    },
+    { "chan": 17,   "name": "C_field",        "signed": -128,   "scale": 1.920,   "offset": 0    },
+    { "chan": 18,   "name": "int_N2_HT_U",    "signed": -128,   "scale": 0.048,   "offset": 0    },
+    { "chan": 19,   "name": "int_N2_HT_I",    "signed": -128,   "scale": 19.00,   "offset": 0    },
+    { "chan": 20,   "name": "int_N1_HT_U",    "signed": -128,   "scale": 0.048,   "offset": 0    },
+    { "chan": 21,   "name": "int_N1_HT_I",    "signed": -128,   "scale": 19.00,   "offset": 0    },
+    { "chan": 22,   "name": "ext_HT_U",       "signed": -128,   "scale": 0.048,   "offset": 0    },
+    { "chan": 23,   "name": "ext_HT_I",       "signed": -128,   "scale": 19.00,   "offset": 0    },
+    { "chan": 24,   "name": "RF_U",           "signed": -128,   "scale": 0.298,   "offset": 0    },
+    { "chan": 25,   "name": "RF_I",           "signed": -128,   "scale": 0.010,   "offset": 0    },
+    { "chan": 26,   "name": "p24V",           "signed": -128,   "scale": 0.240,   "offset": 0    },
+    { "chan": 27,   "name": "p15V1",          "signed": -128,   "scale": 0.148,   "offset": 0    },
+    { "chan": 28,   "name": "n15V1",          "signed": -128,   "scale": 0.148,   "offset": 0    },
+    { "chan": 29,   "name": "p5V",            "signed": -128,   "scale": 0.148,   "offset": 0    },
+    { "chan": 30,   "name": "p15V2",          "signed": -128,   "scale": 0.148,   "offset": 0    },
+    { "chan": 31,   "name": "n15V2",          "signed": -128,   "scale": 0.148,   "offset": 0    },
+    { "chan": 32,   "name": "OCXO",           "signed": 0,      "scale": 0.078,   "offset": 0    },
+    { "chan": 33,   "name": "Ampl5.7k",       "signed": 0,      "scale": 0.078,   "offset": 0    },
+    { "chan": 34,   "name": "Lock",           "signed": 0,      "scale": 1.000,   "offset": 0    },
+]
 
-with open('EFOS14.json') as f:
-    s = f.read()
-    channels = json.loads(s)
+#with open('EFOS14.json') as f:
+#    s = f.read()
+#    channels = json.loads(s)
     
 def poll_chan(ser, chan):
     s = "D%02d" % chan
@@ -38,15 +74,15 @@ with serial.Serial(SERIALDEVICE, BAUDRATE, timeout=None) as ser:
             r = poll_chan(ser, channel['chan'])
             try:
                 reading = int(r,16)
-                fields[channel['name']] = (reading - channel['signed']) * channel['scale'] + channel['offset']
+                fields[channel['name']] = (reading + channel['signed']) * channel['scale'] + channel['offset']
             except:
                 print("Error converting string: %s", r)
         json_body = [
             {
-                "measurement": "maserdata",
+                "measurement": "EFOS10",
                 "tags": {
                     "masetype": "EFOS-B",
-                    "maser": "EFOS14"
+                    "maser": "EFOS10"
                 },
                 "time": timestamp,
                 "fields": fields
